0d4497eb298a5fd9baf6902185ce53c648e098c4
Walace Silva <wsilva.emp@gmail.com>
2024-11-06 18:26:38 -0300

    refactor: adjust on models

--- app/Models/DataCollection.php
+++ app/Models/DataCollection.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class DataCollection extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'station_id',
+        'collected_at',
+        'rain',
+        'level',
+    ];
 }
--- app/Models/Station.php
+++ app/Models/Station.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use App\Interfaces\Entities\StationDomain;
+use App\Interfaces\Entities\StationEntity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Station extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+
+    /**
+     * Get attributes to station domain
+     */
+    public function toDomain(): StationDomain
+    {
+        return new StationEntity(
+            $this->id,
+            $this->created_at,
+            $this->updated_at,
+            $this->title,
+            $this->code,
+        );
+    }
 }

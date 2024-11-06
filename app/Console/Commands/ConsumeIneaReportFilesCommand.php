6c0d9099e98f3498bf1a3503681d30c73ba7d12f
Walace Silva <wsilva.emp@gmail.com>
2024-11-06 18:27:45 -0300

    refactor: adjust on consumer inea report service

--- app/Console/Commands/ConsumeIneaReportFilesCommand.php
+++ app/Console/Commands/ConsumeIneaReportFilesCommand.php
@@ -2,10 +2,13 @@
 
 namespace App\Console\Commands;
 
+use Exception;
 use App\Jobs\ProcessRabbitMQMessage;
+use App\Services\ReportService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class ConsumeIneaReportFilesCommand extends Command
 {
@@ -30,13 +33,54 @@ class ConsumeIneaReportFilesCommand extends Command
     {
         Log::debug('Hourly task started.');
 
-        $folderPath = 'server';
+        $this->handleFilesFromStorage('/CIEM/Telemetria/');
 
-        // $files = Storage::disk('local')->files($folderPath);
+        Log::debug('Hourly task finished.');
+    }
+
+    /**
+     * Handle files from storage.
+     *
+     * @param string $folderPath The folder path.
+     * @return void
+     */
+    private function handleFilesFromStorage($folderPath = '')
+    {
+        $fileSystem = Storage::disk('ftp');
+        $files = $fileSystem->files($folderPath);
+
+        foreach ($files as $file) {
+            if (!$fileSystem->getVisibility($file) === 'public') {
+                continue;
+            }
+
+            try {
+                $fileContent = $fileSystem->get($file);
+    
+                Log::info('File content: {content}', ['content' => $fileContent]);
+    
+                if ($fileContent) {
+                    ReportService::processReport($fileContent);
+                    // $data = ['content' => $fileContent];
+                    // ProcessRabbitMQMessage::dispatch($data);
+                }
+            } catch(Exception $exception) {
+                Log::error('Error processing file: {file}', ['file' => $file, 'exception' => $exception->getMessage()]);
+            }
+        }
+    }
+
+    /**
+     * Handle files from local.
+     *
+     * @param string $folderPath The folder path.
+     * @return void
+     */
+    private function handleFilesFromLocal($folderPath = '')
+    {
         $files = File::files($folderPath);
 
         foreach ($files as $file) {
-            // $fileContent = Storage::disk('local')->get($file);
             $fileContent = File::get($file);
 
             Log::info('File content: {content}', ['content' => $fileContent]);
@@ -46,7 +90,5 @@ class ConsumeIneaReportFilesCommand extends Command
                 ProcessRabbitMQMessage::dispatch($data);
             }
         }
-
-        Log::debug('Hourly task finished.');
     }
 }
--- app/Jobs/ProcessRabbitMQMessage.php
+++ app/Jobs/ProcessRabbitMQMessage.php
@@ -7,9 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
-use App\Exceptions\ParseErrorException;
-use App\Exceptions\FormattingErrorException;
+use App\Services\ReportService;
 
 class ProcessRabbitMQMessage implements ShouldQueue
 {
@@ -25,36 +23,15 @@ class ProcessRabbitMQMessage implements ShouldQueue
     public function __construct($data)
     {
         $this->data = $data;
+        $this->onQueue('default');
+        $this->onConnection('rabbitmq');
     }
+
     /**
      * Execute the job.
      */
     public function handle(): void
     {
-        try {
-            Log::debug('Starting to process data: ' . $this->data);
-
-            list($date, $time, $amount) = explode(' ', $this->data);
-            Log::debug('Parsed data - Date: ' . $date . ', Time: ' . $time . ', Amount: ' . $amount);
-
-            $date = \DateTime::createFromFormat('d/m/Y', $date);
-            if (!$date) {
-                throw new ParseErrorException('Invalid date format');
-            }
-            Log::debug('Formatted date: ' . $date->format('Y-m-d'));
-
-            $amount = str_replace(',', '.', $amount);
-            if (!is_numeric($amount)) {
-                throw new FormattingErrorException('Invalid amount format');
-            }
-            Log::debug('Formatted amount: ' . $amount);
-
-            echo "Date: " . $date->format('Y-m-d') . "\n";
-            echo "Time: " . $time . "\n";
-            echo "Amount: " . $amount . "\n";
-        } catch (ParseErrorException | FormattingErrorException $e) {
-            Log::error('Error processing data: ' . $e->getMessage());
-            echo 'Error: ' . $e->getMessage() . "\n";
-        }
+        ReportService::processReport($this->data);
     }
 }

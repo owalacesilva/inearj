3bc17c8ec35dbcc392c14e6a58c9189101e6852b
Walace Silva <wsilva.emp@gmail.com>
2024-12-02 21:40:10 -0300

    feat: create filter to data collections

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -5,16 +5,55 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreStationRequest;
 use App\Http\Requests\UpdateStationRequest;
 use App\Models\Station;
+use Illuminate\Http\Request;
 
 class StationController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $filteredStation = $request->input('station');
+        $filteredStatus = $request->input('status');
+        $filteredDate = $request->input('date');
+        
+        $queryBuilder = Station::select([
+            'stations.title', 
+            'stations.code',
+            'stations.status',
+            'stations.kind_of',
+            'data_collections.id',
+            'data_collections.created_at',
+            'data_collections.collected_at',
+            'data_collections.station_id',
+            'data_collections.rain',
+            'data_collections.level',
+            ])
+            ->leftJoin('data_collections', 'data_collections.station_id', '=', 'stations.id');
+
+        if (isset($filteredStation) && !is_null($filteredStation)) {
+            $queryBuilder->when($filteredStation, function ($query, $filteredStation) {
+                return $query->where('stations.code', $filteredStation);
+            });
+        }
+
+        if (isset($filteredDate) && !is_null($filteredDate)) {
+            $queryBuilder->when($filteredDate, function ($query, $filteredDate) {
+                return $query->whereDate('data_collections.collected_at', $filteredDate);
+            });
+        }
+
+        $queryBuilder->orderBy('stations.title', 'asc');
+        $queryBuilder->groupBy('stations.code');
+        
+        $stations = $queryBuilder->get();
+
         return view('station.index', [
-            'stations' => Station::all(),
+            'stations' => $stations,
+            'filteredStation' => $filteredStation,
+            'filteredStatus' => $filteredStatus,
+            'filteredDate' => $filteredDate,
         ]);
     }
 
--- resources/views/station/index.blade.php
+++ resources/views/station/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
+            <form class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
                 <div class="col-span-1 md:col-span-1">
                     <div class="">
                         <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -15,7 +15,7 @@
                         <select id="station-filter" name="station" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option value="">{{ __('Todas as estações') }}</option>
                             @foreach($stations as $station)
-                                <option value="{{ $station->id }}">{{ $station->title }}</option>
+                                <option value="{{ $station->code }}" @selected($filteredStation == $station->code)>{{ $station->title }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -27,10 +27,9 @@
                         </label>
                         <select id="status-filter" name="status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option value="">{{ __('Todos os Status') }}</option>
-                            <option value="ATUALIZADO">{{ __('Atualizado') }}</option>
-                            <option value="ATRASADO">{{ __('Atrasado') }}</option>
-                            <option value="ALERTA">{{ __('Alerta') }}</option>
-                            <option value="MANUTENÇÃO">{{ __('Manutenção') }}</option>
+                            <option value="up" @selected($filteredStatus == 'up')>{{ __('Atualizado') }}</option>
+                            <option value="alert" @selected($filteredStatus == 'alert')>{{ __('Atrasado') }}</option>
+                            <option value="warning" @selected($filteredStatus == 'warning')>{{ __('Alerta') }}</option>
                         </select>
                     </div>
                 </div>
@@ -39,20 +38,23 @@
                         <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                             {{ __('Filtrar por data') }}
                         </label>
-                        <input type="date" id="date-filter" name="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <input type="date" id="date-filter" name="date" value="{{ $filteredDate }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     </div>
                 </div>
-                <div class="col-span-1 md:col-span-1 flex items-center justify-center">
-                    <div class="flex items-center justify-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="animate-spin h-5 w-5 text-gray-900 dark:text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <polyline points="23 4 23 10 17 10"></polyline>
-                            <polyline points="1 20 1 14 7 14"></polyline>
-                            <path d="M3.51 9a9 9 0 0114.36-3.36L23 10M1 14l5.64 5.64A9 9 0 0020.49 15"></path>
-                        </svg>
-                        <span class="text-sm font-medium text-gray-900 dark:text-white">{{ __('Sincronizando niveis') }}</span>
+                <div class="col-span-1 md:col-span-1">
+                    <div class="">
+                        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            <span>&nbsp;</span>
+                        </label>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                            Filtrar
+                        </button>
+                        <button type="reset" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                            Limpar
+                        </button>
                     </div>
                 </div>
-            </div>
+            </form>
             <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -111,10 +113,10 @@
                                     {{ $station->kind_of }}
                                 </td>
                                 <td class="py-4 px-6">
-                                    {{ \Carbon\Carbon::parse($station->installation_date)->format('d/m/Y') }}
+                                    {{ \Carbon\Carbon::parse($station->collected_at)->format('d/m/Y') }}
                                 </td>
                                 <td class="py-4 px-6">
-                                    200,00 Litros
+                                    {{ $station->level }} Litros
                                 </td>
                                 <td class="py-4 px-6">
                                     <a href="{{ route('station_details', ['code' => $station->code]) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{{ __('Detalhes') }}</a>
